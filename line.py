a1c6a54df33835e808f9fe582f8ca41994031b3c
ssd <aa@aaa>
2019-01-30 16:40:15 +0900

    'change'

--- line.py
+++ line.py
@@ -21,12 +21,9 @@ book_link = [x.select_one('div.detail > div.title > a').text for x in book_list]
 book_title = [x.select_one('div.detail > div.title > a > strong').text for x in book_list]
 book_price = [x.select_one('div.detail > div.price > strong').text for x in book_list]
 book_eprice = [x.select_one('div.detail > div.price > a ').text for x in book_list]
-print("☆ ☆ ☆ ☆ ☆ 이번주 베스트셀러 순위 ☆ ☆ ☆ ☆ ☆")
+# print("☆ ☆ ☆ ☆ ☆ 이번주 베스트셀러 순위 ☆ ☆ ☆ ☆ ☆")
 
 data = {}
-
-for i in range (0,20):
-    a = print(i+1,"위",book_title[i] , " => 도서 : ['" ,book_price[i], "'] ebook : ", book_eprice[i].split())
 # try:
 #     for book in range(0,20):
 #         link = 'http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf'
@@ -40,10 +37,25 @@ API_HOST = 'https://notify-api.line.me'
 
 url = 'https://notify-api.line.me/api/notify'
 
-token = {'Authorization': 'Bearer wpRFnajjEO4opdWimvXXuxLoKjY7kKhRJFifjgUgjHN'}
+token = {'Authorization': 'Bearer mtOi5sJjEjozZGA9KELM2H5Im61uq4Rt6zdiKqCD3AT'}
 
 message = []
-parameter = {"message": message}
+message.append("\n☆ ☆ 이번주 베스트셀러 순위 ☆ ☆"+"\n\n")
+for book in range(0,20):
+    # message.append([book]+"")
+    inner = []
+    inner.append(str(book+1)+"위 : ")
+    inner.append(book_title[book]+"\n")
+    inner.append(book_price[book])
+    inner.append(book_eprice[book]+"\n")
+    content = inner[0] + inner[1] + inner[2] + inner[3]
+    message.append(content)
+
+    # a = print(i+1,"위",book_title[i] , " => 도서 : ['" ,book_price[i], "'] ebook : ", book_eprice[i].split())
+string = str()
+for elem in message:
+    string += elem
+parameter = {"message": string}
 # parameter = {"message": message, "stickerId":2, "stickerPackageId":100}
 
 
@@ -51,4 +63,4 @@ parameter = {"message": message}
 response = requests.post(
     url, headers =token, data = parameter)
 
-# print(response.text)
+print(response.text)
